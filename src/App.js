2356e62583023dc8cdce152accb47a073b454632
Omer Ram <0meram95@gmail.com>
2022-05-26 09:58:37 +0300

    format number to price

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import "ag-grid-community/dist/styles/ag-theme-alpine.css";
 const App = () => {
   const [gridApi, setGridApi] = useState(null);
   const [gridColumnApi, setGridColumnApi] = useState(null);
-  const [hideColumn, setHideColumn] = useState(false);
+  const [hideColumn, setHideColumn] = useState(true);
 
   const rowData = useMemo(
     () => [
@@ -16,8 +16,8 @@ const App = () => {
       { type: "Toyota", model: "Celica", price: 35000, name: "omer", id: 1 },
       { type: "Porsche", model: "Kadi", price: 72000, name: "golan", id: 4 },
       { type: "Porsche", model: "Wlkswagon", price: 72000, name: "nim", id: 5 },
-      { type: "Porsche", model: "Hundai", price: 72000, name: "peter", id: 6 },
-      { type: "Porsche", model: "Kaia", price: 72000, name: "yaakov", id: 7 },
+      { type: "Porsche", model: "Hundai", price: 720, name: "peter", id: 6 },
+      { type: "Porsche", model: "Kaia", price: 980000, name: "yaakov", id: 7 },
       { type: "Porsche", model: "Mits", price: 72000, name: "fritz", id: 8 },
     ],
     []
@@ -30,7 +30,7 @@ const App = () => {
           headerName: "User",
           width: 100,
           children: [
-            { field: "id", width: 50 },
+            { field: "id", width: 70 },
             { field: "name", width: 100 },
           ],
         },
@@ -43,7 +43,14 @@ const App = () => {
         },
         {
           headerName: "Money",
-          children: [{ field: "price", width: 100, hide: true }],
+          children: [
+            {
+              field: "price",
+              width: 150,
+              hide: true,
+              valueFormatter: currencyFormatter,
+            },
+          ],
         },
       ],
       defaultColDef: {
@@ -58,16 +65,26 @@ const App = () => {
     setGridColumnApi(params.columnApi);
   }
 
-  const showColumn = () => {
+  function showColumn() {
     gridColumnApi.setColumnVisible("price", hideColumn);
     setHideColumn(!hideColumn);
     gridApi.sizeColumnsToFit();
-  };
+  }
+
+  function currencyFormatter(params) {
+    return formatNumber(params.value) + "$";
+  }
+
+  function formatNumber(number) {
+    return Math.floor(number)
+      .toString()
+      .replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,");
+  }
 
   return (
     <div
       className="ag-theme-alpine"
-      style={{ height: 400, width: 550, margin: 50 }}
+      style={{ height: 450, width: 550, margin: 50 }}
     >
       <h1>MY Grid</h1>
       <AgGridReact
@@ -76,7 +93,9 @@ const App = () => {
         defaultColDef={columns.defaultColDef}
         onGridReady={onGridReady}
       ></AgGridReact>
-      <button onClick={showColumn}>show price</button>
+      <button onClick={showColumn} style={{ marginTop: 10 }}>
+        {hideColumn ? "show price" : "hide price"}
+      </button>
     </div>
   );
 };
